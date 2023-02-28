ff9af2f72449c906c8c1bd12b9fc535a49b9474d
Anushka Sharma <as0846403@gmail.com>
2023-02-28 18:57:40 +0530

    first commit

---
+++ android/app/src/main/kotlin/com/example/techbot/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.techbot
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/constants/api_consts.dart
@@ -0,0 +1,2 @@
+String BASE_URL = "https://api.openai.com/v1";
+String API_KEY = "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";
